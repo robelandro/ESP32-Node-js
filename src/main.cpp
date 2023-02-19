2d76a9c17298f2163b6605dda81fd530c1408560
robelpopandro <robelpopandro@gmail.com>
2023-02-19 11:10:32 -0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,89 +1,140 @@
 #include <WiFi.h>
-#include <WebSocketClient.h>
+#include <WebSocketsClient.h>
 #include <ArduinoJson.h>
 
-const char* ssid     = "POPLs";
-const char* password = "12345678n";
-char path[] = "/";
-char host[] = "192.168.43.158:5000";
-  
-WebSocketClient webSocketClient;
+const char* ssid = "POPLs";
+const char* password = "infopops";
+const char* host = "192.168.43.158"; // IP address of the host to connect to
+int port = 5000; // port to use for the websocket connection
+
+int ThermistorPin = 34, SwPin = 33, FlamePin = 32, FanPin = 23, LightPin = 22;
+float Vo;
+float R1 = 10000.0;
+float temprature, Tc, in_put_vol;
+
 WiFiClient client;
+WebSocketsClient webSocket;
 
-void setup() {
-  Serial.begin(115200);
-  delay(10);
+float adc(float bins){
+	return (3.3/4095.0) * bins;
+}
 
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+float temp_fun(){
+  Vo = analogRead(ThermistorPin);
+  // Measured
+  //Calculating Temprature
+  in_put_vol = (float) adc(Vo);
+  temprature =  (4050.0 / (12.42 - log(in_put_vol)));
+  Tc = temprature - 273.0;
+  Serial.print("Temprature in celisus: ");
+  Serial.println(Tc);
+  return Tc;
+}
+
+void fan(){
+  if (temp_fun() > 30){
+    digitalWrite( FanPin, HIGH);
   }
-
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  delay(5000);
-  
-
-  if (client.connect("192.168.43.158", 5000)) {
-    Serial.println("Connected");
-  } else {
-    Serial.println("Connection failed.");
-    while(1) {
-      // Hang on failure
-    }
-  }
-
-  webSocketClient.path = path;
-  webSocketClient.host = host;
-  if (webSocketClient.handshake(client)) {
-    Serial.println("Handshake successful");
-  } else {
-    Serial.println("Handshake failed.");
-    while(1) {
-      // Hang on failure
-    }  
+  else{
+    digitalWrite( FanPin, LOW);
   }
 }
 
-void loop() {
-  if (client.connected()) {
-    String data;
-    webSocketClient.getData(data);
+void switcher(){
+  if (digitalRead(SwPin)){
+    digitalWrite(LightPin, HIGH);
+  }
+  else{
+    digitalWrite(LightPin, LOW);
+  }
+}
 
-    if (data.length() > 0) {
-      StaticJsonDocument<200> doc;
-      DeserializationError error = deserializeJson(doc, data);
-      if (!error) {
-        Serial.println("Received JSON: ");
-        serializeJson(doc, Serial);
-        Serial.println("");
-      } else {
-        Serial.println("Error parsing received JSON");
+void flame_fun(){
+  if (digitalRead(FlamePin)){
+    Serial.println("there is flame");
+  }
+  else{
+    Serial.println("there is flame");
+  }
+}
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.println("Disconnected");
+      break;
+    case WStype_CONNECTED:
+      Serial.println("Connected");
+      break;
+    case WStype_TEXT:
+      StaticJsonDocument<200> doc;                    // create a JSON container
+      DeserializationError error = deserializeJson(doc, payload);
+      if (error) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+        return;
       }
-    }
+      else {
+        // JSON string was received correctly, so information can be retrieved:
+        const char* message = doc["message"];
+        Serial.println("Received user: ");
+        Serial.println("message: " + String(message));
 
-    StaticJsonDocument<200> doc;
-    doc["message"] = "Hello from ESP32";
-    String jsonString;
-    serializeJson(doc, jsonString);
-    webSocketClient.sendData(jsonString);
-    
-  } else {
-    Serial.println("Client disconnected.");
-    while (1) {
-      // Hang on disconnect.
-    }
+      }
+      Serial.println("");
+      break;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  pinMode(LightPin, OUTPUT);
+  pinMode(FanPin, OUTPUT);
+  pinMode(FlamePin, INPUT);
+  pinMode(SwPin, INPUT);
+  
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
   }
 
-  delay(3000);
+  Serial.println();
+  Serial.println("WiFi connected");
+
+  webSocket.begin(host, port, "/");
+  webSocket.onEvent(webSocketEvent);
+
+  while (webSocket.isConnected()) {
+    Serial.println("Connecting to websocket server...");
+    delay(1000);
+    webSocket.begin(host, port, "/");
+    webSocket.onEvent(webSocketEvent);
+  }
+
+  Serial.println("Websocket connected");
+}
+
+void loop() {
+  webSocket.loop();
+
+  fan();
+  switcher();
+  flame_fun();
+
+  if (webSocket.isConnected()) {
+    Serial.println("Sending data...");
+    String jsonString = "";                           // create a JSON string for sending data to the client
+    StaticJsonDocument<200> doc;                      // create a JSON container
+    JsonObject object = doc.to<JsonObject>();         // create a JSON Object
+    object["message"] = random(100);                    // write data into the JSON object -> I used "rand1" and "rand2" here, but you can use anything else
+    serializeJson(doc, jsonString);                   // convert JSON object to string
+    // Serial.println(jsonString);                       // print JSON string to console for debug purposes (you can comment this out)
+    webSocket.sendTXT(jsonString);               // send JSON string to clients
+    delay(1000);
+  }
 }
